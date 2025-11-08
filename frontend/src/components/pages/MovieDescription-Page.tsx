f229b19e751feb442e55cd333d88c1a5860db4db
connorWinningUM <cew8t4@umsystem.edu>
2025-11-08 16:21:53 -0600

    Dummy Trailer iframe

--- frontend/src/components/pages/MovieDescription-Page.tsx
+++ frontend/src/components/pages/MovieDescription-Page.tsx
@@ -23,7 +23,7 @@ function MovieDescription(){
             linear-gradient(to bottom, 
                 transparent 0%, 
                 transparent 50%,  
-                var(--color-5) 100%
+                var(--black) 100%
             ),
             url(https://image.tmdb.org/t/p/original${movie.backdrop_path})
         `,
@@ -32,25 +32,51 @@ function MovieDescription(){
         backgroundRepeat: 'no-repeat',
         }
     : {};
+
+    const movieDetails = (
+        <div className=' flexbox items-center'>
+            <p className="text-6xl font-bold wrap-normal movie-title">{movie.title}</p>
+            <p className="text-xl wrap-normal movie-title movie-title movie-subtitle">{movie.release_date}</p>
+            <MovieRatings vote_average={movie.vote_average} vote_count={movie.vote_count} />
+        </div>
+    );
+
+    const poster = (
+        <img
+            src={'https://image.tmdb.org/t/p/w500' + movie.poster_path}
+            alt={movie.title}
+            className="movie-img"
+        />
+    );
+
+
+    var trailerKey = "M_HjrYiwS2I"
+    var trailerSrc = "https://www.youtube.com/embed/" + trailerKey;
+    const trailer = (
+        <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '1rem'}}>
+            <iframe
+                width="560"
+                height="315"
+                src={trailerSrc}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                />
+        </div>
+    );
     
     return (
         <div className='MovieDescription' style={backgroundStyle}>
             <div className="max-w-7xl mx-auto px-4 py-6 container ">
                 <div className="grid grid-cols-3">
                     <div className="col-span-1">
-                        <img
-                            src={'https://image.tmdb.org/t/p/w500' + movie.poster_path}
-                            alt={movie.title}
-                            className="movie-img"
-                        />
+                        {poster}
                     </div>
                     <div className='col-span-2'>
                         <div className='movie-details'>
-                            <div className=' flexbox items-center'>
-                                <p className="text-6xl font-bold wrap-normal movie-title">{movie.title}</p>
-                                <p className="text-xl wrap-normal movie-title movie-title movie-subtitle">{movie.release_date}</p>
-                                <MovieRatings vote_average={movie.vote_average} vote_count={movie.vote_count} />
-                            </div>
+                            {movieDetails}
+                            {trailer}
                         </div>
                     </div>
                 </div>
