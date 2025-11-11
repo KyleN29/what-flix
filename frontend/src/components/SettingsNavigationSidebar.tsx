a5349dfaf38e44731f04675bc0a0de0a9c6e89a7
connorWinningUM <cew8t4@umsystem.edu>
2025-11-11 01:41:45 -0600

    basic navbar

---
+++ frontend/src/components/SettingsNavigationSidebar.tsx
@@ -0,0 +1,22 @@
+
+function NavigationSidebar() {
+    const general = (
+        <details>
+            <summary>General</summary>
+            <p>SETTINGS</p>
+        </details>
+    )
+    
+    return(
+        <div className="nav-sidebar"> 
+            {general}
+            {general}
+            {general}
+            {general}
+            {general}
+            {general}
+        </div>
+    )
+}
+
+export default NavigationSidebar
