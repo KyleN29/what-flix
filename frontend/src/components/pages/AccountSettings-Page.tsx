24f8773d78097a6b85cf116a12c497dab538ace8
connorWinningUM <cew8t4@umsystem.edu>
2025-11-11 01:41:45 -0600

    basic navbar

--- frontend/src/components/pages/AccountSettings-Page.tsx
+++ frontend/src/components/pages/AccountSettings-Page.tsx
@@ -1,15 +1,17 @@
+import "./AccountSettings-Page.css"
+import NavigationSidebar from "../SettingsNavigationSidebar";
 
 function AccountSettings() {
     const settingsNav = (
-        <div className="col-span-1">
-            NAVBAR
+        <div className="col-span-1 AccountNavigationBar">
+            <NavigationSidebar />
         </div>
     );
 
     return (
-        <div className="grid grid-cols-4">
+        <div className="grid grid-cols-5 AccountSettings">
             {settingsNav}
-            <div className="col-span-3">
+            <div className="col-span4">
                 <h1>CONTENT GOES HERE</h1>
             </div>
         </div>
