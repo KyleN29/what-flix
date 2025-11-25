3ff435badddc1ee8eba3245e5bb1e3c0a32972e2
connorWinningUM <cew8t4@umsystem.edu>
2025-11-25 17:15:58 -0600

    css changes to settings navigation sidebar

--- frontend/src/components/SettingsNavigationSidebar.tsx
+++ frontend/src/components/SettingsNavigationSidebar.tsx
@@ -1,21 +1,37 @@
 import { ChevronDown, Home, Settings, Users, FileText, BarChart, Mail, Bell, Shield } from 'lucide-react';
+import "./SettingsNavigationSidebar.css"
 
 function NavigationSidebar() {
-    const general = (
-        <details>
-            <summary>General</summary>
-            <p>SETTINGS</p>
-        </details>
-    )
+
+    const heading = (
+        <div className='flex nav-sidebar-heading'>
+            ACCOUNT DETAILS
+        </div>
+    );
     
     return(
-        <div className="nav-sidebar"> 
-            {general}
-            {general}
-            {general}
-            {general}
-            {general}
-            {general}
+        <div className="nav-sidebar-container flex flex-col"> 
+            {heading}
+            <div className='nav-sidebar-item'>
+                <h1>SECTION TITLE</h1>
+                <h2>DETAILS</h2>
+            </div>
+            <div className='nav-sidebar-item'>
+                <h1>SECTION TITLE</h1>
+                <h2>DETAILS</h2>
+            </div>
+            <div className='nav-sidebar-item'>
+                <h1>SECTION TITLE</h1>
+                <h2>DETAILS</h2>
+            </div>
+            <div className='nav-sidebar-item'>
+                <h1>SECTION TITLE</h1>
+                <h2>DETAILS</h2>
+            </div>
+            <div className='nav-sidebar-item'>
+                <h1>SECTION TITLE</h1>
+                <h2>DETAILS</h2>
+            </div>
         </div>
     )
 }
