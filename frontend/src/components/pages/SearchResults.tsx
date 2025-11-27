c7ea6d122773ee44aff52783b4846d99af0797ba
Kyle Noelker <gcarey0@outlook.com>
2025-11-27 04:53:21 -0600

    Add basic search results page

---
+++ frontend/src/components/pages/SearchResults.tsx
@@ -0,0 +1,37 @@
+import { useSearchParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import type { Movie } from '../../services/MovieService';
+import MovieService from '../../services/MovieService';
+
+function SearchResults() {
+  const [params] = useSearchParams();
+  const query = params.get('q') || '';
+  const page = Number(params.get('page') || 1);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['search', query, page],
+    queryFn: () => MovieService.searchMovies(query, page),
+    enabled: !!query
+  });
+
+  if (!query) return <p>No search term entered.</p>;
+  if (isLoading) return <p>Loading search results…</p>;
+  if (error) return <p>Error loading search results.</p>;
+
+  return (
+    <div>
+      <h2>Search Results for: "{query}"</h2>
+
+      <div className="movie-grid">
+        {data.map((movie: Movie) => (
+          <div key={movie.id}>
+            <h3>{movie.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SearchResults;
--- frontend/src/components/pages/index.tsx
+++ frontend/src/components/pages/index.tsx
@@ -1,2 +1,3 @@
 export { default as MovieDescription } from './MovieDescription-Page';
 export { default as Login } from './Login';
+export { default as SearchResults} from './SearchResults'
