ff7900a8e12fcdb09fe15cc2860f9abec05d1094
connorWinningUM <cew8t4@umsystem.edu>
2025-12-01 13:01:55 -0600

    cleaning up merge

--- frontend/src/components/pages/index.tsx
+++ frontend/src/components/pages/index.tsx
@@ -1,4 +1,5 @@
 export { default as MovieDescription } from './MovieDescription-Page';
 export { default as Login } from './Login';
 export { default as SearchResults} from './SearchResults';
+export { default as AccountSettings} from './AccountSettings-Page';
 export { default as AccountSettings } from './AccountSettings-Page';
