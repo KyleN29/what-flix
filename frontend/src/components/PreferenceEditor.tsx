39a333e32b1f4f7c36f1d7535af1858f09e55eb8
KyleN29 <gcarey0@outlook.com>
2025-12-03 23:28:28 -0600

    Implement tab switching

--- frontend/src/components/PreferenceEditor.tsx
+++ frontend/src/components/PreferenceEditor.tsx
@@ -1,47 +1,51 @@
-
 import { usePreferenceEditor } from '../context/PreferenceEditorContext';
 import { useEffect, useState } from 'react';
-import './PreferenceEditor.css'
-
+import './PreferenceEditor.css';
 
 function PreferenceEditor() {
-    const {isOpen, closeEditor} = usePreferenceEditor()
-      const [visible, setVisible] = useState(false);
+  const { isOpen, closeEditor } = usePreferenceEditor();
+  const [visible, setVisible] = useState(false);
 
-    
-    useEffect(() => {
-        if (isOpen) {
-        requestAnimationFrame(() => setVisible(true));
-        } else {
-        setVisible(false);
-        }
-    }, [isOpen]);
-    if (!isOpen) return;
+  const tabs = ['Genres', 'Actors', 'Favorite Movies'];
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  useEffect(() => {
+    if (isOpen) {
+      requestAnimationFrame(() => setVisible(true));
+    } else {
+      setVisible(false);
+    }
+  }, [isOpen]);
+  if (!isOpen) return;
 
   return (
     <>
-    <div className={`preference-editor-backdrop ${visible ? "animate-in" : ""}`} onClick={closeEditor}></div>
-    <div className={`preference-editor-container ${visible ? "animate-in" : ""}`}>
+      <div
+        className={`preference-editor-backdrop ${visible ? 'animate-in' : ''}`}
+        onClick={closeEditor}
+      ></div>
+      <div
+        className={`preference-editor-container ${visible ? 'animate-in' : ''}`}
+      >
         <div className="preference-editor">
-            <div className="preference-editor-header header-gradient">
-                Personalize Your Experience
-            </div>
-            <div className="preference-editor-tabs">
-                <div className="active-tab">
-                    Genres
-                </div>
-                <div>
-                    Actors
-                </div>
-                <div>
-                    Favorite Movies
-                </div>
-            </div>
-            <div className="preference-editor-body">
-                Personalize Your Experience
-            </div>
+          <div className="preference-editor-header header-gradient">
+            Personalize Your Experience
+          </div>
+          <div className="preference-editor-tabs">
+            {tabs.map((tabName) => (
+              <div
+                className={`tab ${activeTab == tabName ? 'active-tab' : ''}`}
+                onClick={() => setActiveTab(tabName)}
+              >
+                {tabName}
+              </div>
+            ))}
+          </div>
+          <div className="preference-editor-body">
+            Personalize Your Experience
+          </div>
         </div>
-    </div>
+      </div>
     </>
   );
 }
