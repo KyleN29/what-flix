5a2d166646b4907bf30c7b7490a0dbc5d84438e4
KyleN29 <gcarey0@outlook.com>
2025-12-05 01:32:32 -0600

    Fix bugs from main and have server serve frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,23 +1,29 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from "mongoose"
-// import movieRoutes from './routes/movieRoutes.js';
 import accountCommandController from './commandService/controllers/accountCommandController.js'
 import accountQueryController from './queryService/controllers/accountQueryController.js'
 import genreQueryController from './queryService/controllers/genreQueryController.js'
 import movieQueryController from './queryService/controllers/movieQueryController.js'
 import searchSuggestionQueryController from './queryService/controllers/searchSuggestionQueryController.js'
 import eventHandler from './queryService/handlers/eventHandler.js'
-// import MovieService, {type Movie} from './services/MovieService.js';
-// import { MovieModel } from './models/Movie.js';
 import dotenv from "dotenv";
 dotenv.config();
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const distPath = path.join(__dirname, "..", "..", "frontend", "dist");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors())
+app.use(express.static(distPath));
+
 mongoose
   .connect(process.env.MONGO_URI as string)
   .then(() => console.log("MongoDB connected"))
@@ -26,8 +32,8 @@ mongoose
   });
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
 app.use('/events', eventHandler)
