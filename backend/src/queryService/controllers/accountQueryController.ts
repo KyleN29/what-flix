d8177530ed91d7efd1720f7422a95f8e0d33435b
KyleN29 <gcarey0@outlook.com>
2025-12-04 23:17:13 -0600

    Add backend endpoint for getting a user's genre rankings

--- backend/src/queryService/controllers/accountQueryController.ts
+++ backend/src/queryService/controllers/accountQueryController.ts
@@ -1,5 +1,6 @@
 import { Router, type Request, type Response } from 'express';
 import accountQueryService from '../services/accountQueryService.js'
+import GenrePreferencesRead from '../models/GenrePreferencesRead.js';
 
 const router = Router();
 
@@ -19,6 +20,16 @@ router.get('/:id/genre_ranking/:genre_code', async (req: Request, res: Response)
   const genrePreference = preferences.find(pref => pref.genre_code === genreCode);
   res.json(genrePreference);
 });
+
+router.get('/genre_ranking', authMiddleware, async (req: Request, res: Response) => {
+  const userId = req.user.user_id;
+  const genres = req.body;
+
+  const prefs = await GenrePreferencesRead.findOne({user_id: userId})
+
+  res.json(prefs?.genre_rankings);
+});
+
 
 router.get('/:id/movie_rating/:movie_id', async (req: Request, res: Response) => {
   const movieRating = await accountQueryService.getMovieRanking(req.params.id, req.params.movie_id);
