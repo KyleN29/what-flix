5d5c130346763c8ca1d19e5f8c156abf47e2127b
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2025-12-05 19:48:17 -0600

    Updated login page css

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
-import AuthService, {type LoginPaylod, type RegisterPayload} from '../../services/AuthService';
+import AuthService, {
+  type LoginPaylod,
+  type RegisterPayload
+} from '../../services/AuthService';
+import MovieService, { type Movie } from '../../services/MovieService';
 import './../variables.css';
 import './Login.css';
-import { useAuth } from "../../context/AuthContext";
-
+import { useAuth } from '../../context/AuthContext';
 
 function Login() {
   const { setIsLoggedIn } = useAuth();
@@ -17,10 +20,11 @@ function Login() {
   const [registerRepeatPassword, setRegisterRepeatPassword] = useState('');
 
   const registerMutation = useMutation({
-    mutationFn: (formData: RegisterPayload) => AuthService.registerUser(formData),
+    mutationFn: (formData: RegisterPayload) =>
+      AuthService.registerUser(formData),
     onSuccess: (data) => {
       console.log('Registered successfully!');
-      localStorage.setItem('accessToken', data.accessToken)
+      localStorage.setItem('accessToken', data.accessToken);
       setIsLoggedIn(true);
     },
     onError: (err) => {
@@ -32,7 +36,7 @@ function Login() {
     mutationFn: (formData: LoginPaylod) => AuthService.loginUser(formData),
     onSuccess: (data) => {
       console.log('Login successfully!');
-      localStorage.setItem('accessToken', data.accessToken)
+      localStorage.setItem('accessToken', data.accessToken);
       setIsLoggedIn(true);
     },
     onError: (err) => {
@@ -43,14 +47,13 @@ function Login() {
   const handleLoginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-
     loginMutation.mutate({
       email: loginEmail,
       password: loginPassword
     });
   };
 
-    const handleRegisterSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleRegisterSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (registerPassword !== registerRepeatPassword) {
@@ -65,14 +68,11 @@ function Login() {
     });
   };
 
-
-  
-
-
   return (
     <div className="login-page">
-      <h3>Login:</h3>
+      <div className="background"></div>
       <form className="login" onSubmit={handleLoginSubmit}>
+        <h3>Login</h3>
         <p>Username:</p>
         <input
           type="text"
@@ -90,8 +90,8 @@ function Login() {
         <button type="submit">Submit</button>
       </form>
       <br />
-      <h3>Create Account:</h3>
       <form className="create-account" onSubmit={handleRegisterSubmit}>
+        <h3>Create Account</h3>
         <p>Email:</p>
         <input
           type="text"
