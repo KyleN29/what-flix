657f9f89b16d3fd8dba07f149b7ccc805258c5fc
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2025-12-14 16:18:54 -0600

    Updated test_render_service.py to be more clear

--- tests/test_render_service.py
+++ tests/test_render_service.py
@@ -1,7 +1,10 @@
+# Tests if the render service is working properly.
+
 from selenium import webdriver
 import time
 
 TITLE_GOAL = "What-Flix"
+TITLE_LOADING = "Render - Application Loading"
 
 def test_render_service():
   driver = webdriver.Chrome()
@@ -9,7 +12,10 @@ def test_render_service():
 
   if driver.title == TITLE_GOAL:
     assert True
-  else:
-    print("\nRender may be asleep due to inactivity. Waiting 60 seconds then trying again.")
+  elif driver.title == TITLE_LOADING:
+    print("\nRender application was asleep due to inactivity. Waiting 60 seconds then trying again.")
     time.sleep(60)
-    assert (driver.title == TITLE_GOAL)
+    driver.get("https://what-flix.onrender.com/")
+    assert driver.title == TITLE_GOAL, f"Webpage title \"{driver.title}\" does not match TITLE_GOAL of \"{TITLE_GOAL}\"."
+  else:
+    assert False, f"Webpage title \"{driver.title}\" does not match TITLE_GOAL of \"{TITLE_GOAL}\" or TITLE_LOADING of \"{TITLE_LOADING}\"."
