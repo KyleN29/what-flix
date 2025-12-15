142a52ff2d1e0a10210651c9a7780f0803aa9e5c
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2025-12-14 20:21:29 -0600

    testing if api key is working correctly

--- tests/test_tmdb_api.py
+++ tests/test_tmdb_api.py
@@ -2,7 +2,9 @@ import requests
 import os
 
 def test_tmdb_api():
-  url = f"https://api.themoviedb.org/3/movie/26963?api_key={os.getenv('TMDB_API_KEY')}"
+  api_key = os.getenv('TMDB_API_KEY')
+  print(str(api_key)[:3])
+  url = f"https://api.themoviedb.org/3/movie/26963?api_key={api_key}"
   data = requests.get(url).json()
   print(data)
 
