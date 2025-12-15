763fcfcc74cc2d010be90e59e4b48f35be245f69
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2025-12-14 21:46:25 -0600

    attempt at validating api key

--- tests/test_tmdb_api.py
+++ tests/test_tmdb_api.py
@@ -3,7 +3,7 @@ import os
 
 def test_tmdb_api():
   api_key = os.getenv('TMDB_API_KEY')
-  print(str(api_key)[:3])
+  print(f"\n\nAPI KEY:::{str(api_key)[:3]}\n\n")
   url = f"https://api.themoviedb.org/3/movie/26963?api_key={api_key}"
   data = requests.get(url).json()
   print(data)
